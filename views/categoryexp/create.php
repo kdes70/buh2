f2639b2eac7344a9a0be197b8deb01ea732d297b
Timur Melnikov <melnikovt@gmail.com>
2015-05-07 14:45:25 +0300

    Update create.php

--- views/categoryexp/create.php
+++ views/categoryexp/create.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Categoryexp */
 
-$this->title = 'Создание ';
+$this->title = 'Создание';
 $this->params['breadcrumbs'][] = ['label' => 'Категории расходов', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 
