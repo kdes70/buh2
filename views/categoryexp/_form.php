cbe43fad763486645598ca95de61dd20f6e0f287
timur <timur@home-lin64>
2015-05-10 22:59:27 +0300

    Работа над разделом "Расходы" 1

--- views/categoryexp/_form.php
+++ views/categoryexp/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Categoryexp */
@@ -12,7 +13,7 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'parent_id')->dropDownList(app\models\Categoryexp::getAllForSelect(2)) ?>
+    <?= $form->field($model, 'parent_id')->dropDownList(ArrayHelper::map(app\models\Categoryexp::getAllForSelect(2), 'id', 'name')) ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => 20]) ?>
 
