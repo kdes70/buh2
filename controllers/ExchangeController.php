1c5e61fb9d7e8637e6897147b32bd7def33b7e16
timur <timur@melnikov>
2015-05-27 12:48:45 +0300

    Добавление словаря "Курсы валют" 2

--- controllers/ExchangeController.php
+++ controllers/ExchangeController.php
@@ -8,14 +8,14 @@ use app\models\ExchangeSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\helpers\Json;
 
 /**
  * ExchangeController implements the CRUD actions for Exchange model.
  */
-class ExchangeController extends Controller
-{
-    public function behaviors()
-    {
+class ExchangeController extends Controller {
+
+    public function behaviors() {
         return [
             'verbs' => [
                 'class' => VerbFilter::className(),
@@ -30,14 +30,13 @@ class ExchangeController extends Controller
      * Lists all Exchange models.
      * @return mixed
      */
-    public function actionIndex()
-    {
+    public function actionIndex() {
         $searchModel = new ExchangeSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
+                    'searchModel' => $searchModel,
+                    'dataProvider' => $dataProvider,
         ]);
     }
 
@@ -46,10 +45,9 @@ class ExchangeController extends Controller
      * @param integer $id
      * @return mixed
      */
-    public function actionView($id)
-    {
+    public function actionView($id) {
         return $this->render('view', [
-            'model' => $this->findModel($id),
+                    'model' => $this->findModel($id),
         ]);
     }
 
@@ -58,15 +56,14 @@ class ExchangeController extends Controller
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
      */
-    public function actionCreate()
-    {
+    public function actionCreate() {
         $model = new Exchange();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('create', [
-                'model' => $model,
+                        'model' => $model,
             ]);
         }
     }
@@ -77,15 +74,14 @@ class ExchangeController extends Controller
      * @param integer $id
      * @return mixed
      */
-    public function actionUpdate($id)
-    {
+    public function actionUpdate($id) {
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('update', [
-                'model' => $model,
+                        'model' => $model,
             ]);
         }
     }
@@ -96,8 +92,7 @@ class ExchangeController extends Controller
      * @param integer $id
      * @return mixed
      */
-    public function actionDelete($id)
-    {
+    public function actionDelete($id) {
         $this->findModel($id)->delete();
 
         return $this->redirect(['index']);
@@ -110,12 +105,33 @@ class ExchangeController extends Controller
      * @return Exchange the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
-    protected function findModel($id)
-    {
+    protected function findModel($id) {
         if (($model = Exchange::findOne($id)) !== null) {
             return $model;
         } else {
             throw new NotFoundHttpException('The requested page does not exist.');
         }
     }
+
+    //Получение значений курсов валют...
+    //public function actionGetExchange($currency_code, $start_date) {
+    public function actionGetexchange() {
+
+        //$location = Locations::findOne($zipId);
+
+        $data = <<<JSON
+        [{"ccy":"RUR","base_ccy":"UAH","buy":"0.41000","sale":"0.45000"},{"ccy":"EUR","base_ccy":"UAH","buy":"22.00000","sale":"24.50000"},{"ccy":"USD","base_ccy":"UAH","buy":"20.00000","sale":"22.50000"}]        
+JSON;
+
+
+
+        $json = file_get_contents('https://api.privatbank.ua/p24api/exchange_rates?json&date=10.05.2015');
+
+
+
+        echo Json::encode($json);
+        //echo $data;
+        exit;
+    }
+
 }
