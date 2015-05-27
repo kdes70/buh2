4b7e22b193741682554eb6cc28122d16c0370736
timur <timur@home-lin64>
2015-05-27 13:51:37 +0300

    Добавление словаря "Курсы валют" 5

--- views/exchange/_form.php
+++ views/exchange/_form.php
@@ -1,8 +1,8 @@
 <?php
 
 use yii\helpers\Html;
-//use yii\widgets\ActiveForm;
 use kartik\widgets\ActiveForm;
+use dosamigos\datepicker\DatePicker;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Exchange */
@@ -13,20 +13,30 @@ use kartik\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'start_date')->textInput() ?>
+    <?=
+    $form->field($model, 'start_date')->widget(
+            DatePicker::className(), [
+        // inline too, not bad
+        'inline' => FALSE,
+        'language' => 'ru',
+        // modify template for custom rendering
+        //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+        'clientOptions' => [
+            'autoclose' => true,
+            'format' => 'yyyy-mm-dd'
+        ]
+    ]);
+    ?>
 
     <?= $form->field($model, 'currency_code')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'number_units')->textInput() ?>
 
-
-
-
     <?=
     $form->field($model, 'official_exchange', [
         'addon' => [
             'append' => [
-                'content' => Html::button('Получить...', ['class' => 'btn btn-primary', 'id'=>'get-exchange']),
+                'content' => Html::button('Получить...', ['class' => 'btn btn-primary', 'id' => 'get-exchange']),
                 'asButton' => true
             ]
         ]
@@ -34,9 +44,6 @@ use kartik\widgets\ActiveForm;
 
     //http://demos.krajee.com/widget-details/active-field#addon
     ?>
-
-
-
 
 
     <div class="form-group">
