885b96843f0bc3e269432130b7fd068fde44a525
timur <timur@melnikov>
2015-06-02 10:31:41 +0300

    Верстка главной страницы

--- views/main/index.php
+++ views/main/index.php
@@ -37,52 +37,50 @@ use kartik\growl\Growl;
 
 
     </div>
+
+
     <div class="col-md-6 col-lg-6">
-
-
-
-
-
         <!-- Блок управления -->
+<!--        <style type="text/css">
+            .custab{
+                border: 1px solid #ccc;
+                //padding: 5px;
+                //margin: 5% 0;
+                box-shadow: 3px 3px 2px #ccc;
+                transition: 0.5s;
+            }
+            .custab:hover{
+                box-shadow: 3px 3px 0px transparent;
+                transition: 0.5s;
+            }
+            td, th{
 
-
-
-
-
-
-
-        <!--
-                <style type="text/css">
-                    .custab{
-                        border: 1px solid #ccc;
-                        padding: 5px;
-                        margin: 5% 0;
-                        box-shadow: 3px 3px 2px #ccc;
-                        transition: 0.5s;
-                    }
-                    .custab:hover{
-                        box-shadow: 3px 3px 0px transparent;
-                        transition: 0.5s;
-                    }
-                </style>-->
-
+                border: 1px solid #ccc; 
+            }
+        </style>-->
 
 
         <table class="table table-striped custab">
             <thead>
-
                 <tr>
-                    <th>ID</th>
+                    <th colspan="1" width="100%">Раздел</th>
+                    <th colspan="2" class="text-center">Действие</th>
 
-                    <th>Раздел</th>
-                    <th class="text-center">Действие</th>
                 </tr>
             </thead>
             <tr>
-                <td>1</td>
+                <td><?= Html::a('<span class="glyphicon glyphicon-eye-open"></span> Кошельки', ['/wallet'], ['class' => 'btn btn-success btn-block']) ?></td>
 
-                <td>Кошельки</td>
-                <td class="text-right"><a class='btn btn-primary' href="#"><span class="glyphicon glyphicon-plus-sign"></span> Добавить</a>         <div class="btn-group" role="group">
+
+                <td class="text-right">
+
+
+                    <?= Html::a('<span class="glyphicon glyphicon-plus-sign"></span> Добавить', ['/wallet/create'], ['class' => 'btn btn-primary']) ?>         
+                </td>
+                <td class="text-right"> 
+
+                    <div class="btn-group" role="group">
+
                         <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
                             <span class="glyphicon glyphicon-edit"></span> Шаблон
                             <span class="caret"></span>
@@ -91,114 +89,12 @@ use kartik\growl\Growl;
                             <li><a href="#">Dropdown link</a></li>
                             <li><a href="#">Dropdown link</a></li>
                         </ul>
-                    </div></td>
+                    </div>
+                </td>
             </tr>
-
-
-            <tr>
-                <td>2</td>
-
-                <td>Доходы</td>
-                <td class="text-right"><a class='btn btn-primary' href="#"><span class="glyphicon glyphicon-plus-sign"></span> Добавить</a>         <div class="btn-group" role="group">
-                        <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                            <span class="glyphicon glyphicon-edit"></span> Шаблон
-                            <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Dropdown link</a></li>
-                            <li><a href="#">Dropdown link</a></li>
-                        </ul>
-                    </div></td>
-            </tr>
-
-            <tr>
-                <td>3</td>
-
-                <td>Расходы</td>
-                <td class="text-right"><a class='btn btn-primary' href="#"><span class="glyphicon glyphicon-plus-sign"></span> Добавить</a>         <div class="btn-group" role="group">
-                        <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                            <span class="glyphicon glyphicon-edit"></span> Шаблон
-                            <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Dropdown link</a></li>
-                            <li><a href="#">Dropdown link</a></li>
-                        </ul>
-                    </div></td>
-            </tr>
-
-            <tr>
-                <td>4</td>
-
-                <td>Переводы</td>
-                <td class="text-right"><a class='btn btn-primary' href="#"><span class="glyphicon glyphicon-plus-sign"></span> Добавить</a>         <div class="btn-group" role="group">
-                        <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                            <span class="glyphicon glyphicon-edit"></span> Шаблон
-                            <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Dropdown link</a></li>
-                            <li><a href="#">Dropdown link</a></li>
-                        </ul>
-                    </div></td>
-            </tr>
-            
-            
-            <tr>
-                <td>5</td>
-
-                <td>Шаблоны</td>
-                <td class="text-right"><a class='btn btn-primary' href="#"><span class="glyphicon glyphicon-plus-sign"></span> Добавить</a>         <div class="btn-group" role="group">
-                        <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                            <span class="glyphicon glyphicon-edit"></span> Шаблон
-                            <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Dropdown link</a></li>
-                            <li><a href="#">Dropdown link</a></li>
-                        </ul>
-                    </div></td>
-            </tr>
-
         </table>
-
-
-
         <!-- Блок управления (конец) -->
-
-
-
-
-
-
-        <?php
-//        echo yii\bootstrap\Tabs::widget([
-//            'items' => [
-//                [
-//                    'label' => 'Расходы',
-//                    //'content' => $exp,
-//                    'content' => $this->render('_expense', ['dataProvider' => $dataProviderExpense, 'searchModel' => $searchModelExpense, 'model' => $modelExpense]),
-//                    'options' => ['id' => 'myveryownID'],
-//                    'active' => true
-//                ],
-//                [
-//                    'label' => 'Доходы',
-//                    'content' => $this->render('_income', ['dataProvider' => $dataProviderIncome, 'searchModel' => $searchModelIncome, 'model' => $modelIncome]),
-//                    //'headerOptions' => [...],
-//                    'options' => ['id' => 'myveryownID1'],
-//                ],
-//                [
-//                    'label' => 'Переводы',
-//                    'content' => 'Переводы...',
-//                    //'headerOptions' => [...],
-//                    'options' => ['id' => 'myveryownID2'],
-//                ],
-//            ],
-//        ]);
-        ?>
-
     </div>
-
     <div class="col-md-3 col-lg-3">
 
 
