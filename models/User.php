9e184592e825886201bb4a93d19ee7568e7aa34b
timur <timur@melnikov>
2015-07-29 11:47:16 +0300

    Создание RBAC (регистрация юзера)

--- models/User.php
+++ models/User.php
@@ -40,6 +40,17 @@ class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface {
     }
 
     //Мои методы...
+
+    /**
+     * @inheritdoc
+     */
+    public function rules() {
+        return [
+            [['id', 'created_at', 'updated_at', 'status'], 'integer'],
+            [['username', 'auth_key', 'email_confirm_token', 'password_hash', 'password_reset_token', 'email'], 'string'],
+        ];
+    }
+
     /**
      * @inheritdoc
      */
@@ -50,6 +61,43 @@ class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface {
             'username' => 'Логин',
             'status' => 'Статус'
         ];
+    }
+
+    /**
+     * Хешируем пароль, перед сохранением Generates password hash from password and sets it to the model
+     *
+     * @param string $password
+     */
+    public function setPassword($password) {
+        
+        
+        
+        $this->password_hash = Yii::$app->security->generatePasswordHash($password);
+    }
+
+    /**
+     * Generates "remember me" authentication key
+     */
+    public function generateAuthKey() {
+        $this->auth_key = Yii::$app->security->generateRandomString();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function beforeSave($insert) {
+        if (parent::beforeSave($insert)) {
+            if ($insert) {
+                
+                //Эта строка уберется, будет поле PASSWORD
+                $this->setPassword($this->password_hash); 
+                //Эта строка уберется, будет поле PASSWORD (конец)
+                
+                $this->generateAuthKey();
+            }
+            return true;
+        }
+        return false;
     }
 
 }
