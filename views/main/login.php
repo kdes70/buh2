50a82eafe8a35c22a71e88bf91523b1c4c7a8041
timur <timur@melnikov>
2015-08-05 11:19:01 +0300

    Дизайн формы входа

--- views/main/login.php
+++ views/main/login.php
@@ -13,22 +13,22 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
 
-   
+
 
     <?php
     $form = ActiveForm::begin([
                 'id' => 'login-form',
                 'options' => ['class' => 'form-horizontal'],
                 'fieldConfig' => [
-                'template' => "{label}\n<div class=\"col-sm-3 col-md-3 col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-                'checkboxTemplate' => "<div class=\"col-lg-offset-1 col-sm-3 col-md-3 col-lg-3\">{beginLabel}\n{input}\n{labelTitle}\n{endLabel}</div>",
-                'labelOptions' => ['class' => 'col-lg-1 control-label'],
-            ],
-        ]);
+                    'template' => "{label}\n<div class=\"col-sm-3 col-md-3 col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
+                    'checkboxTemplate' => "<div class=\"col-lg-offset-1 col-sm-3 col-md-3 col-lg-3\">{beginLabel}\n{input}\n{labelTitle}\n{endLabel}</div>",
+                    'labelOptions' => ['class' => 'col-lg-1 control-label'],
+                ],
+    ]);
     ?>
 
-    <?= $form->field($model, 'username')->textInput(['value'=>'timur']) ?>
-    <?= $form->field($model, 'password')->passwordInput(['value'=>'111'])?>
+    <?= $form->field($model, 'username')->textInput(['value' => 'timur']) ?>
+    <?= $form->field($model, 'password')->passwordInput(['value' => '111']) ?>
     <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
     <div class="form-group">
@@ -40,3 +40,82 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?php ActiveForm::end(); ?>
 </div>
+
+
+
+
+
+<div class="container" style="margin-top:40px">
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-md-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <strong> Вход в программу</strong>
+                </div>
+                <div class="panel-body">
+                    <form role="form" action="#" method="POST">
+                        <fieldset>
+                            <div class="row">
+                                <div class="center-block">
+                                    <img class="profile-img"
+                                          src="/buh2/web/images/key.png">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12 col-md-10  col-md-offset-1 ">
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <span class="input-group-addon">
+                                                <i class="glyphicon glyphicon-user"></i>
+                                            </span> 
+                                            <input class="form-control" placeholder="Имя пользователя" name="loginname" type="text" autofocus>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <span class="input-group-addon">
+                                                <i class="glyphicon glyphicon-lock"></i>
+                                            </span>
+                                            <input class="form-control" placeholder="Пароль" name="password" type="password" value="">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="submit" class="btn btn-lg btn-primary btn-block" value="Войти">
+                                    </div>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+<!--                <div class="panel-footer ">
+                    Don't have an account! <a href="#" onClick=""> Sign Up Here </a>
+                </div>-->
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+
+    .panel-heading {
+        padding: 5px 15px;
+    }
+
+    .panel-footer {
+        padding: 1px 15px;
+        color: #A0A0A0;
+    }
+
+    .profile-img {
+/*        width: 96px;
+        height: 96px;*/
+        margin: 0 auto 10px;
+        display: block;
+        -moz-border-radius: 50%;
+        -webkit-border-radius: 50%;
+        border-radius: 50%;
+    }
+
+</style>
+
+
